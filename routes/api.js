3852c817c27dbcec06bce5b44afd6e0cbdf4585b
Dhakshin Krishna <78342331+Dozerex-creationz@users.noreply.github.com>
2022-01-05 20:05:28 +0530

    updated routes until latest edit

--- routes/api.js
+++ routes/api.js
@@ -5,9 +5,39 @@ const Translator = require('../components/translator.js');
 module.exports = function (app) {
   
   const translator = new Translator();
-
+  
   app.route('/api/translate')
     .post((req, res) => {
-      
+      const locale=req.body.locale;
+      const text=req.body.text;
+      let ans="";
+      let trans=""
+      if(!text || !locale){
+        ans={ error: 'Required field(s) missing' }
+      }
+      if(text==''){
+        ans={ error: 'No text to translate' }
+      }
+      else if(locale!="american-to-british" && locale!="british-to-american"){
+        ans={ error: 'Invalid value for locale field' }
+      }
+      else{      
+      switch(locale){
+        case "american-to-british":
+        {
+          trans=translator.aTb(text)
+          ans={text:text,translation:trans}
+        }
+        case "british-to-american":
+        {
+          trans=translator.bTa(text)
+          ans={text:text,translation:trans}
+        }
+        default:
+        {
+          ans={ error: 'Invalid value for locale field' }
+        }
+      }}
+      res.json(ans);        
     });
 };
