68e114fbc178aecc14d7a4e5786fcf886042c5a0
Dhakshin Krishna <78342331+Dozerex-creationz@users.noreply.github.com>
2022-01-05 20:04:46 +0530

    updated the code with improper finishes

--- components/translator.js
+++ components/translator.js
@@ -1,10 +1,43 @@
-const americanOnly = require('./american-only.js');
-const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+'use strict';
 
-class Translator {
+const Translator = require('../components/translator.js');
 
-}
-
-module.exports = Translator;
+module.exports = function (app) {
+  
+  const translator = new Translator();
+  
+  app.route('/api/translate')
+    .post((req, res) => {
+      const locale=req.body.locale;
+      const text=req.body.text;
+      let ans="";
+      let trans=""
+      if(!text || !locale){
+        ans={ error: 'Required field(s) missing' }
+      }
+      if(text==''){
+        ans={ error: 'No text to translate' }
+      }
+      else if(locale!="american-to-british" && locale!="british-to-american"){
+        ans={ error: 'Invalid value for locale field' }
+      }
+      else{      
+      switch(locale){
+        case "american-to-british":
+        {
+          trans=translator.aTb(text)
+          ans={text:text,translation:trans}
+        }
+        case "british-to-american":
+        {
+          trans=translator.bTa(text)
+          ans={text:text,translation:trans}
+        }
+        default:
+        {
+          ans={ error: 'Invalid value for locale field' }
+        }
+      }}
+      res.json(ans);        
+    });
+};
